d801e70cd8ad47b5f9413da359625ca704661e06
Pedro-Moretti <moretti.drope@gmail.com>
2024-04-10 20:44:46 -0300

    Feat: update pet by id use case: gerenciador de erros e input

--- src/pet/interfaces/pet.repository.interface.ts
+++ src/pet/interfaces/pet.repository.interface.ts
@@ -3,4 +3,5 @@ import { Pet } from "../schemas/pet.schema";
 export default interface IPetRepository {
     create(data: Partial<Pet>): Promise<Pet>
     getById(id: string): Promise<Pet>
+    updateById(data: Partial<Pet>): Promise<void>
 }
--- src/pet/usecases/update.pet.by.id.usecase.ts
+++ src/pet/usecases/update.pet.by.id.usecase.ts
@@ -1,12 +1,53 @@
 import { IUseCase } from "src/domain/iusecase.interface";
 import UpdatePetByIdUseCaseInput from "./dtos/update.pet.by.id.usecase.input";
 import UpdatePetByIdUseCaseOutput from "./dtos/update.pet.by.id.usecase.output";
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
+import PetTokens from "../pet.token";
+import { Pet } from "../schemas/pet.schema";
+import PetNotFoundError from "src/domain/errors/pet.not.found.error";
+import IPetRepository from "../interfaces/pet.repository.interface";
 
 @Injectable()
 export default class UpdatePetByIdUseCase implements IUseCase<UpdatePetByIdUseCaseInput, UpdatePetByIdUseCaseOutput> {
-    run(input: UpdatePetByIdUseCaseInput): Promise<UpdatePetByIdUseCaseOutput> {
-        throw new Error("Method not implemented.");
+
+    constructor(
+        @Inject(PetTokens.petRepository)
+        private readonly petRepository: IPetRepository
+    ) { }
+
+    async run(input: UpdatePetByIdUseCaseInput): Promise<UpdatePetByIdUseCaseOutput> {
+        
+        let pet = await this.getPetById(input.id)
+
+        if(!pet) {
+            throw new PetNotFoundError()
+        }
+
+        await this.petRepository.updateById({
+            ...input
+        });
+
+        pet = await this.getPetById(input.id);
+
+        return new UpdatePetByIdUseCaseOutput({
+            id: pet._id,
+            name: pet.name,
+            type: pet.type,
+            size: pet.size,
+            gender: pet.gender,
+            bio: pet.bio,
+            photo: pet.photo,
+            createdAt: pet.createdAt,
+            updatedAt: pet.updatedAt,
+        })
     }
-    
+
+    private async getPetById(id: string): Promise<Pet> {
+        try {
+            return await this.petRepository.getById(id)
+        } catch (error) {
+            return null
+        }
+    }
 }
+    
